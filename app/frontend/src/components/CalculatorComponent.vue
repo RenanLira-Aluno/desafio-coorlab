68bfb9d3f1e1e519f705b11c58da1fa3b173b333
RenanLira-Aluno <catce.20191ads0401@aluno.ifpi.edu.br>
2024-03-20 19:21:49 -0300

    Calculator Component add form

---
+++ app/frontend/src/components/CalculatorComponent.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { PhTruck, PhHandCoins } from '@phosphor-icons/vue'
+const props = defineProps<{
+    title: string
+    subtitle: string
+}>()
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="header">
+            <PhTruck :weight="'fill'" :size="40" />
+            <h2>
+                {{ props.title }}
+            </h2>
+        </div>
+        <div class="body">
+            <form>
+                <div class="form-title">
+                    <PhHandCoins :weight="'fill'" />
+                    <h3>{{ props.subtitle }}</h3>
+                </div>
+
+                <div class="form-section">
+                    <label for="distance">Destino</label>
+                    <input type="number" id="destination" />
+                </div>
+
+                <div class="form-section">
+                    <label for="date">Date</label>
+                    <input type="date" id="date" />
+                </div>
+
+                <div class="form-section">
+                    <button type="submit">Calcular</button>
+                </div>
+            </form>
+            <div class="result">
+                <p>Nenhum Dado Selecionado</p>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.header {
+    @apply flex w-full h-20 bg-slate-500 text-white items-center gap-4 px-10 text-2xl font-bold;
+}
+
+.container {
+    @apply flex flex-col w-full h-full shadow-xl rounded-md overflow-hidden;
+}
+
+.body {
+    @apply flex gap-4 px-4 py-10 h-full;
+}
+
+form {
+    @apply flex flex-col gap-4 p-20 bg-slate-300 flex-1 justify-center;
+}
+
+.form-section {
+    @apply flex flex-col gap-2;
+}
+
+.form-title {
+    @apply flex items-center gap-2;
+}
+
+.result {
+    @apply flex flex-1
+}
+</style>
--- app/frontend/src/views/CalculatorView.vue
+++ app/frontend/src/views/CalculatorView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-
+import Calculator from '../components/CalculatorComponent.vue'
 </script>
 
 
 <template>
-    <div>
-        <h1>Calculadora de Viagem</h1>
+    <div class="page">
+        <Calculator title="Calculadora de Viagem" subtitle="Calcule o Valor da Viagem" />
     </div>
 </template>
+
+<style scoped>
+.page {
+    @apply flex w-full items-center justify-center px-32 py-40;
+}
+</style>
