b1f459815240f9b75264993e0f399d0831be72db
RenanLira-Aluno <catce.20191ads0401@aluno.ifpi.edu.br>
2024-03-23 19:01:33 -0300

    stop run.sh services

--- app/run.sh
+++ app/run.sh
@@ -2,7 +2,17 @@
 cd backend
 pip3 install -r requirements.txt
 nohup python3 -m flask --app app.py run --port 3000 --debug > log.txt 2>&1 &
+echo $! > ../save_pid.txt
 
 cd ../frontend
 npm install
 nohup npm run dev > log.txt 2>&1 &
+echo $! >> ../save_pid.txt
+
+echo "frontend e backend rodando em segundo plano."
+echo "http://localhost:8080/"
+echo "http://localhost:3000/"
+echo "logs em backend/log.txt e frontend/log.txt."
+
+echo "Para parar os processos, execute:"
+echo "kill -9 cat save_pid.txt"
