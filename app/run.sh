f01faec1f3dec4f39f740718ff1d1c13f9667e68
RenanLira-Aluno <catce.20191ads0401@aluno.ifpi.edu.br>
2024-03-23 17:59:52 -0300

    run.sh script implemented

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+cd backend
+pip3 install -r requirements.txt
+nohup python3 -m flask --app app.py run --port 3000 --debug > log.txt 2>&1 &
 
-echo "Implemente aqui o script para executar a sua solução"
+cd ../frontend
+npm install
+nohup npm run dev > log.txt 2>&1 &
