5df79fcc7f7aa0340d674db5bf4be2bea62b43fc
RenanLira-Aluno <catce.20191ads0401@aluno.ifpi.edu.br>
2024-03-23 17:21:50 -0300

    auto complete dropdown button styled

--- app/frontend/src/components/AutoCompleteComponent.vue
+++ app/frontend/src/components/AutoCompleteComponent.vue
@@ -30,13 +30,14 @@ const search = (event: AutoCompleteCompleteEvent) => {
 <template>
     <div>
         <label :for="id">{{ label }}</label>
-        <AutoComplete :input-id="id" v-model="selectInput" dropdown force-selection
-            v-on:item-select="change" :suggestions="filteredOptions" @complete="search" :pt="{
+        <AutoComplete :input-id="id" v-model="selectInput" dropdown
+            dropdown-class="absolute right-3 items-center text-center flex flex-row-reverse" rce-selection v-on:item-select="change"
+            :suggestions="filteredOptions" @complete="search" :pt="{
             input: 'bg-white p-3 h-12 focus:outline-none w-full rounded-md overflow-hidden',
             list: 'bg-white mt-3 p-4 flex flex-col gap-4 overflow-y-scroll max-h-60 w-full shadow-md rounded-md',
             root: 'flex relative items-center',
-            loadingIcon: 'absolute right-3',
-            dropdownButton: 'absolute right-3',
+            loadingIcon: 'absolute right-8',
+            container: 'w-full bg-black',
             item: 'cursor-pointer hover:bg-gray-100 p-2 rounded-md transition-colors duration-200 ease-in-out',
 
         }" />
