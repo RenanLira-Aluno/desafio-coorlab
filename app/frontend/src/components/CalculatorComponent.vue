b299a9a6bd3ab1bd5a86a9d4735ab40a12b66731
RenanLira-Aluno <catce.20191ads0401@aluno.ifpi.edu.br>
2024-03-23 16:51:02 -0300

    added error dialog modal

--- app/frontend/src/components/CalculatorComponent.vue
+++ app/frontend/src/components/CalculatorComponent.vue
@@ -7,6 +7,7 @@ import AutoCompleteComponent from './AutoCompleteComponent.vue';
 import DateSelectComponent from './DateSelectComponent.vue';
 import { AutoCompleteItemSelectEvent } from 'primevue/autocomplete';
 import CardComponent from './CardComponent.vue';
+import ErrorDialog from './ErrorDialog.vue';
 
 const props = defineProps<{
     title: string
@@ -18,6 +19,7 @@ const props = defineProps<{
 const cities = ref<string[]>([])
 const selectedCity = ref<string>('')
 const selectedDate = ref<string>('')
+const inError = ref<boolean>(false)
 
 const resultData = ref<IGetBetterTripsResponse | undefined>(undefined)
 
@@ -29,7 +31,7 @@ onMounted(async () => {
 
 
 // event functions
-const change = (event: AutoCompleteItemSelectEvent) => {
+const citySelect = (event: AutoCompleteItemSelectEvent) => {
     selectedCity.value = event.value
 }
 
@@ -37,8 +39,18 @@ const dateSelect = (event: Event) => {
     selectedDate.value = (event.target as HTMLInputElement)?.value
 }
 
+const onCloseError = () => {
+    console.log('close')
+    inError.value = false
+}
+
 const onSubmit = async (event: Event) => {
     event.preventDefault()
+
+    if (!selectedCity.value || !selectedDate.value) {
+        inError.value = true
+        return
+    }
 
     const res = await GetBetterTripsService.get(selectedCity.value, selectedDate.value)
 
@@ -57,12 +69,13 @@ const onSubmit = async (event: Event) => {
         <div class="body">
             <form :on-submit="onSubmit" action="/calculadora">
                 <div class="form-title">
-                    <PhHandCoins :weight="'fill'" />
+                    <PhHandCoins :weight="'fill'" :size="50" />
                     <h3>{{ props.subtitle }}</h3>
                 </div>
 
                 <div class="form-section">
-                    <AutoCompleteComponent :options="cities" :change="change" label="Selecione uma cidade" id="city" />
+                    <AutoCompleteComponent :options="cities" :change="citySelect" label="Selecione uma cidade"
+                        id="city" />
                 </div>
 
                 <div class="form-section">
@@ -83,6 +96,7 @@ const onSubmit = async (event: Event) => {
                 <p>Nenhum dado selecionado.</p>
             </div>
         </div>
+        <ErrorDialog :visible="inError" :close="onCloseError" />
     </div>
 
 </template>
@@ -102,7 +116,7 @@ const onSubmit = async (event: Event) => {
 }
 
 form {
-    @apply flex flex-col gap-8 p-20 bg-slate-300 justify-center rounded-md;
+    @apply flex flex-col gap-8 px-20 bg-slate-300 justify-center rounded-md;
 }
 
 .form-section {
@@ -110,11 +124,11 @@ form {
 }
 
 .form-title {
-    @apply flex items-center gap-4 text-2xl justify-center font-bold;
+    @apply flex items-center gap-4 text-3xl justify-center font-bold;
 }
 
 .result {
-    @apply flex flex-col shadow-xl justify-center items-center p-8 rounded-md w-full gap-8;
+    @apply flex flex-col justify-center items-center p-8 rounded-md w-full gap-8;
 }
 
 .wait-result {
---
+++ app/frontend/src/components/ErrorDialog.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { PhWarningCircle } from '@phosphor-icons/vue';
+import Dialog from 'primevue/dialog';
+
+const { visible, close } = defineProps<{ visible: boolean, close: () => void }>()
+
+</script>
+
+<template>
+    <Dialog :visible="visible" modal :closable="false" :style="{ width: '32vw' }" :pt="{
+        root: 'p-4 bg-white flex justify-between items-center flex-col rounded-md h-96',
+        footer: 'p-4 w-full',
+        mask: 'bg-black bg-opacity-50'
+    }">
+        <div class="content">
+            <PhWarningCircle :size="80" :weight="'fill'" />
+            <p>Insira os valores para carregar a cotação.</p>
+        </div>
+        <template #footer>
+            <button type="button" @click="close">Fechar</button>
+        </template>
+    </Dialog>
+</template>
+
+<style scoped>
+button {
+    @apply p-2 bg-slate-400 text-white rounded-md w-full font-bold text-lg;
+}
+
+.content {
+    @apply flex flex-col items-center gap-4;
+}
+
+p {
+    @apply text-2xl font-bold;
+}
+</style>
